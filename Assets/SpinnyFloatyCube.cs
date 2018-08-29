7e36fa9bc7e12c63056e936fdf4a15bfa085f2a6
Heather Kemp <heather-kemp@uiowa.edu>
2018-08-29 18:38:26 -0500

    Add base project with player obejct and spinning/deleting cube

---
+++ Assets/SpinnyFloatyCube.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinnyFloatyCube : MonoBehaviour {
+
+	public float speed = 10f;
+	private Rigidbody gameObjectsRigidBody;
+	public GameObject player;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Rotate(Vector3.up * speed * Time.deltaTime);
+
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+		if (col.gameObject == player ) {
+			if (gameObjectsRigidBody == null) {
+				gameObjectsRigidBody = this.gameObject.AddComponent<Rigidbody> ();
+				gameObjectsRigidBody.mass = 5; 
+			}
+		} else {
+			Destroy (this.gameObject);
+		}
+
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# EXPCamp-Sept1-2018
+A project for a quick &lt; 30minute introduction to Unity/programming
